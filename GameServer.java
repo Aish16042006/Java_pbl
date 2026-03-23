a20b14ce8e149bfa4f3670dba8279631458be4f8
Aish16042006 <wavevoker16@gmail.com>
2026-03-23 20:43:12 +0530

    Implement GameServer class with anti-cheat functionality

---
+++ GameServer.java
@@ -0,0 +1,37 @@
+package server;
+
+import anticheat.*;
+
+public class GameServer implements Runnable {
+
+    private ActionQueue queue;
+    private boolean running = true;
+    private AntiCheatEngine engine = new AntiCheatEngine();
+
+    public GameServer(ActionQueue queue) {
+        this.queue = queue;
+    }
+
+    public void stop() {
+        running = false;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("🚀 Server Started");
+
+        while (running) {
+            Action action = queue.dequeue();
+
+            if (action != null) {
+                RiskResult result = engine.evaluate(action);
+
+                System.out.println(
+                        "Player " + result.playerId +
+                        " | Status: " + result.status +
+                        " | Rule: " + result.ruleTriggered
+                );
+            }
+        }
+    }
+}
