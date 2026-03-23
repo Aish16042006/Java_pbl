c2d7f8c555022f87879cda0a04d768e0cf2f0e04
Aish16042006 <wavevoker16@gmail.com>
2026-03-23 21:21:42 +0530

    Create RuleViolation.java

---
+++ RuleViolation.java
@@ -0,0 +1,24 @@
+package anticheat;
+
+public class RuleViolation {
+    public final int playerId; // FIXED
+    public final String ruleName;
+    public final SeverityLevel severity;
+    public final long timestamp;
+    public final double actionValue;
+
+    public RuleViolation(int playerId, String ruleName, SeverityLevel severity, long timestamp, double actionValue) {
+        this.playerId = playerId;
+        this.ruleName = ruleName;
+        this.severity = severity;
+        this.timestamp = timestamp;
+        this.actionValue = actionValue;
+    }
+
+    public String toString() {
+        return "[" + ruleName +
+               " | Severity: " + severity +
+               " | Weight: +" + severity.weight +
+               " | Value: " + actionValue + "]";
+    }
+}
